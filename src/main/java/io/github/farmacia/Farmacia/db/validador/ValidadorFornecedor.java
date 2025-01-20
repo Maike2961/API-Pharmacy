0f8e9edb331a606e09c52546d1ec72f40e20e3b8
Maike2961 <mayki.silva@hotmail.com>
2025-01-19 21:40:00 -0300

    feat: validadors das controlles

---
+++ src/main/java/io/github/farmacia/Farmacia/db/validador/ValidadorFornecedor.java
@@ -0,0 +1,30 @@
+package io.github.farmacia.Farmacia.db.validador;
+
+import io.github.farmacia.Farmacia.db.model.Fornecedor;
+import io.github.farmacia.Farmacia.db.repository.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.Optional;
+
+@Component
+public class ValidadorFornecedor {
+
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+
+    public void validar(Fornecedor fornecedor){
+        if(existsFornecedor(fornecedor)){
+            throw new RuntimeException("Fornecedor ja cadastrado com esse local");
+        }
+    }
+
+    public boolean existsFornecedor(Fornecedor fornecedor){
+        Optional<Fornecedor> fornecedor1 = fornecedorRepository.findByCompanhiaAndLocal(
+            fornecedor.getCompanhia(), fornecedor.getLocal());
+
+        if(fornecedor.getId() == null){
+            return fornecedor1.isPresent();
+        }
+        return fornecedor1.map(t -> !t.getId().equals(fornecedor.getId())).orElse(false);
+    }
+}
---
+++ src/main/java/io/github/farmacia/Farmacia/db/validador/ValidadorItem.java
@@ -0,0 +1,32 @@
+package io.github.farmacia.Farmacia.db.validador;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.github.farmacia.Farmacia.db.model.Item;
+import io.github.farmacia.Farmacia.db.repository.ItemRepository;
+
+@Component
+public class ValidadorItem {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    public void validar(Item item){
+        if(existsItem(item)){
+            throw new RuntimeException("Item já cadastrado");
+        }
+    }
+
+    public boolean existsItem(Item item){
+        Optional<Item> byId = itemRepository.findByNomeAndQuantidade(item.getNome(), item.getQuantidade());
+        
+        if(item.getId() == null){
+            return byId.isPresent();
+        }
+        return byId.filter(existingItem -> !item.getId().equals(existingItem.getId())).isPresent();
+    }
+    
+}
