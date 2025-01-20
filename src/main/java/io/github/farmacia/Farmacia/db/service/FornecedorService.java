67fa21c4e19e9d6c146ece3054bea6d3857417ce
Maike2961 <mayki.silva@hotmail.com>
2025-01-19 21:38:38 -0300

    feat: construido as services

---
+++ src/main/java/io/github/farmacia/Farmacia/db/service/FornecedorService.java
@@ -0,0 +1,59 @@
+package io.github.farmacia.Farmacia.db.service;
+
+import io.github.farmacia.Farmacia.db.model.Fornecedor;
+import io.github.farmacia.Farmacia.db.repository.FornecedorRepository;
+import io.github.farmacia.Farmacia.db.repository.ItemRepository;
+import io.github.farmacia.Farmacia.db.validador.ValidadorFornecedor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class FornecedorService {
+
+    @Autowired
+    private FornecedorRepository repository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ValidadorFornecedor validadorFornecedor;
+
+    public Fornecedor salvar(Fornecedor fornecedor){
+        validadorFornecedor.validar(fornecedor);
+        return repository.save(fornecedor);
+    }
+
+    public Optional<Fornecedor> getById(UUID id){
+        return repository.findById(id);
+    }
+
+    public List<Fornecedor> pesquisa(String company){
+        if(company != null){
+            return repository.findByCompanhia(company);
+        }
+        return repository.findAll();
+    }
+
+    public void atualizar(Fornecedor fornecedor){
+        if(fornecedor.getId() == null){
+            throw new IllegalArgumentException("Para Atualizar, é necessario ter um usuário cadastrado");
+        }
+        validadorFornecedor.validar(fornecedor);
+        repository.save(fornecedor);
+    }
+
+    public void deletar(Fornecedor fornecedor){
+        if(possuiItens(fornecedor)){
+            throw new RuntimeException("Fornecedor tem itens");
+        }
+        repository.delete(fornecedor);
+    }
+
+    public boolean possuiItens(Fornecedor fornecedor){
+        return itemRepository.existsByFornecedor(fornecedor);
+    }
+}
---
+++ src/main/java/io/github/farmacia/Farmacia/db/service/ItemService.java
@@ -0,0 +1,53 @@
+package io.github.farmacia.Farmacia.db.service;
+
+import io.github.farmacia.Farmacia.db.model.Item;
+import io.github.farmacia.Farmacia.db.repository.ItemRepository;
+import io.github.farmacia.Farmacia.db.validador.ValidadorItem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemRepository repository;
+
+    @Autowired
+    private ValidadorItem validadorItem;
+
+    public Item salvar(Item item){
+        validadorItem.validar(item);
+        System.out.println(item);
+        return repository.save(item);
+    }
+
+    public Optional<Item> obterPorID(UUID id){
+        return repository.findById(id);
+    }
+
+    public List<Item> pesquisar(String nome){
+        if(nome != null){
+            List<Item> byNome = repository.findByNome(nome);
+            return byNome;
+        }
+        return repository.findAll();
+    }
+
+    public Item atualizar(Item item){
+        if(item.getId() == null){
+            throw new IllegalArgumentException("Para Atualizar, é necessario ter um item cadastrado");
+        }
+        validadorItem.validar(item);
+        return repository.save(item);
+    }
+
+    public void deletar(Item item){
+        repository.delete(item);
+    }
+
+}
