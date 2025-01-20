b373c25e4289ca9453e19d955441bf0dadefc0bc
Maike2961 <mayki.silva@hotmail.com>
2025-01-19 21:40:43 -0300

    feat: configuration do database

---
+++ src/main/java/io/github/farmacia/Farmacia/db/config/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package io.github.farmacia.Farmacia.db.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    String url;
+    @Value("${spring.datasource.username}")
+    String username;
+    @Value("${spring.datasource.password}")
+    String password;
+    @Value("${spring.datasource.driver-class-name}")
+    String driver;
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setDriverClassName(driver);
+
+        hikariConfig.setPoolName("farmacia-db-pool");
+        hikariConfig.setConnectionTestQuery("select 1");
+
+        return new HikariDataSource(hikariConfig);
+    }
+
+}
