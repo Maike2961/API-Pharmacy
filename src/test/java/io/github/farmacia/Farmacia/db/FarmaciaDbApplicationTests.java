05fa225b362c87a17ffbd3991a4cab8fd572d3df
Maike2961 <mayki.silva@hotmail.com>
2025-01-19 21:44:17 -0300

    feat: test files

---
+++ src/test/java/io/github/farmacia/Farmacia/db/FarmaciaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.farmacia.Farmacia.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
