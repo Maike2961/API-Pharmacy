6a4dd77bd827c0401b004f583a519e898c510fd5
Maike2961 <mayki.silva@hotmail.com>
2025-01-19 21:39:22 -0300

    feat: controllers da api

---
+++ src/main/java/io/github/farmacia/Farmacia/db/controller/FornecedorController.java
@@ -0,0 +1,100 @@
+package io.github.farmacia.Farmacia.db.controller;
+
+import io.github.farmacia.Farmacia.db.DTO.FornecedorDTO;
+import io.github.farmacia.Farmacia.db.DTO.PesquisaFornecedorDTO;
+import io.github.farmacia.Farmacia.db.model.Fornecedor;
+import io.github.farmacia.Farmacia.db.service.FornecedorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("fornecedor")
+public class FornecedorController {
+
+    @Autowired
+    private FornecedorService service;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody FornecedorDTO fornecedorDTO){
+        Fornecedor mapear = fornecedorDTO.mapear();
+        service.salvar(mapear);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(mapear.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> obterPorID(@PathVariable("id") String id){
+        UUID uuid = UUID.fromString(id);
+        Optional<Fornecedor> fornecedor = service.getById(uuid);
+
+        if(fornecedor.isPresent()){
+            Fornecedor fornecedor1 = fornecedor.get();
+            FornecedorDTO fornecedorDTO = new FornecedorDTO(
+                    fornecedor1.getCompanhia(),
+                    fornecedor1.getLocal()
+            );
+            return ResponseEntity.ok(fornecedorDTO);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PesquisaFornecedorDTO>> pesquisa(
+            @RequestParam(value = "companhia", required = false) String companhia)
+    {
+        List<Fornecedor> pesquisa = service.pesquisa(companhia);
+        List<PesquisaFornecedorDTO> fornecedorDTOStream =
+                pesquisa.stream().map(
+                        fornecedor -> new PesquisaFornecedorDTO(
+                        fornecedor.getId(), 
+                        fornecedor.getCompanhia(),
+                        fornecedor.getLocal()
+                        )).collect(Collectors.toList());
+        return ResponseEntity.ok(fornecedorDTOStream);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> atualizar(
+            @PathVariable("id") String id,
+            @RequestBody FornecedorDTO fornecedorDTO){
+
+        UUID uuid = UUID.fromString(id);
+
+        Optional<Fornecedor> fornecedor = service.getById(uuid);
+
+        if(fornecedor.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        Fornecedor fornecedor1 = fornecedor.get();
+        fornecedor1.setCompanhia(fornecedorDTO.companhia());
+        fornecedor1.setLocal(fornecedorDTO.local());
+        service.atualizar(fornecedor1);
+        return ResponseEntity.ok(fornecedorDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable("id") String id){
+        UUID uuid = UUID.fromString(id);
+
+        Optional<Fornecedor> fornecedor = service.getById(uuid);
+
+        if(fornecedor.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        service.deletar(fornecedor.get());
+        return ResponseEntity.noContent().build();
+
+    }
+}
---
+++ src/main/java/io/github/farmacia/Farmacia/db/controller/ItemController.java
@@ -0,0 +1,121 @@
+package io.github.farmacia.Farmacia.db.controller;
+
+import io.github.farmacia.Farmacia.db.DTO.FornecedorDTO;
+import io.github.farmacia.Farmacia.db.DTO.ItemDTO;
+import io.github.farmacia.Farmacia.db.DTO.PesquisaItemDTO;
+import io.github.farmacia.Farmacia.db.model.Item;
+import io.github.farmacia.Farmacia.db.repository.FornecedorRepository;
+import io.github.farmacia.Farmacia.db.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("item")
+public class ItemController {
+
+    @Autowired
+    private ItemService service;
+
+    @Autowired
+    private FornecedorRepository repository;
+
+    @PostMapping 
+    public ResponseEntity<Object> salvar(@RequestBody ItemDTO itemDTO){
+        
+        Item mapear = itemDTO.mapear();
+        mapear.setFornecedor(repository.findById(itemDTO.idFornecedor()).orElseThrow(() -> new RuntimeException("Fornecedor não encontrado")));
+
+        service.salvar(mapear);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(mapear.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PesquisaItemDTO>> pesquisa(
+            @RequestParam(value = "nome", required = false) String nome){
+
+        List<Item> pesquisar = service.pesquisar(nome);
+        List<PesquisaItemDTO> resultado = pesquisar.stream()
+                .map(i -> new PesquisaItemDTO(
+                        i.getId(),
+                        i.getNome(),
+                        i.getPrecoCompra(),
+                        i.getPrecoVenda(),
+                        i.getQuantidade(),
+                        i.getDescricao(),
+                        new FornecedorDTO(
+                            i.getFornecedor().getCompanhia(), 
+                            i.getFornecedor().getLocal()
+                        ))).collect(Collectors.toList());
+        return ResponseEntity.ok(resultado);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemDTO> obterPorId(@PathVariable("id") String id){
+
+        UUID uuid = UUID.fromString(id);
+
+        Optional<Item> item = service.obterPorID(uuid);
+
+        if(item.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+
+        Item item1 = item.get();
+
+        ItemDTO itemDTO = new ItemDTO(
+                item1.getNome(),
+                item1.getPrecoCompra(),
+                item1.getPrecoVenda(),
+                item1.getQuantidade(),
+                item1.getDescricao(),
+                item1.getFornecedor().getId()
+        );
+        return ResponseEntity.ok(itemDTO);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ItemDTO> atualizar(@PathVariable("id") String id, @RequestBody ItemDTO itemDTO){
+
+        UUID fromString = UUID.fromString(id);
+
+        Optional<Item> obterPorID = service.obterPorID(fromString);
+
+        if(obterPorID.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        Item item = obterPorID.get();
+        item.setNome(itemDTO.nome());
+        item.setPrecoCompra(itemDTO.precoCompra());
+        item.setPrecoVenda(itemDTO.precoVenda());
+        item.setQuantidade(itemDTO.quantidade());
+        item.setFornecedor(repository.findById(itemDTO.idFornecedor()).orElseThrow(() -> new RuntimeException("Item não encontrado")));
+        service.atualizar(item);
+        return ResponseEntity.ok(itemDTO);
+    
+    }
+  
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletar(@PathVariable("id") String id){
+        UUID fromString = UUID.fromString(id);
+
+        return service.obterPorID(fromString).map(
+            t -> {
+                service.deletar(t);
+                return ResponseEntity.noContent().build();
+            }).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+}
