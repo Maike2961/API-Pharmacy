bece3eba38e7b261a1e834f7a7e89c2f11a106ae
mayki <mayki.silva@hotmail.com>
2025-01-22 16:41:55 -0300

    feat: add entity usuario

---
+++ src/main/java/io/github/farmacia/Farmacia/db/model/Usuario.java
@@ -0,0 +1,32 @@
+package io.github.farmacia.Farmacia.db.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+
+@Entity
+@Table
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "roles")
+    private String roles;
+    
+}
