7c2d30de52c86d61b0026d9816b9176e337b477c
mayki <mayki.silva@hotmail.com>
2025-01-22 16:43:28 -0300

    feat: add repository usuario

---
+++ src/main/java/io/github/farmacia/Farmacia/db/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package io.github.farmacia.Farmacia.db.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import io.github.farmacia.Farmacia.db.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID>{
+    Usuario findByLogin(String login);
+}
