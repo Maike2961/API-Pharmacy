444d41fab506fbf644e6fedf7c3e1501426cdf4f
mayki <mayki.silva@hotmail.com>
2025-01-23 15:27:29 -0300

    feat: add contrutor, atributo e decorator

--- src/main/java/io/github/farmacia/Farmacia/db/model/FornecedorLocais.java
+++ src/main/java/io/github/farmacia/Farmacia/db/model/FornecedorLocais.java
@@ -1,13 +1,27 @@
 package io.github.farmacia.Farmacia.db.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(using = FornecedorLocaisDeserializer.class)
 public enum FornecedorLocais {
-    SAO_PAULO, 
-    BAHIA, 
-    CEARA, 
-    MINAS_GERAIS, 
-    AMAZONAS, 
-    ACRE, 
-    GOIAS, 
-    RIO_DE_JANEIRO, 
-    ESPIRITO_SANTO
+    SAO_PAULO(1L, "sao paulo"), 
+    BAHIA(2L, "bahia"), 
+    CEARA(3L, "ceara"), 
+    MINAS_GERAIS(4L, "minas gerais"), 
+    AMAZONAS(5L, "amazonas"), 
+    ACRE(6L,"acre"), 
+    GOIAS(7L, "goias"), 
+    RIO_DE_JANEIRO(8L, "rio de janeiro"), 
+    ESPIRITO_SANTO(9l, "espirito santo");
+
+    private Long id;
+    private String locais;
+
+    private FornecedorLocais(Long id,String locais){
+        this.id = id;
+        this.locais = locais;
+    }
+
+
+    
 }
